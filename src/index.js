c4f6f2b73bb2805a5a78813e080c432111fc1c45
Victor Andritoiu <devandrito@gmail.com>
2019-12-13 13:00:10 +0100

    bump deps

--- src/index.js
+++ src/index.js
@@ -11,28 +11,37 @@ global.iios_auth = function(Vue) {
   Vue.component('signin', SignIn)
   Vue.component('signup', SignUp)
 
-  Vue.prototype.$services.emit('app:menu:add', [
-    {
-      title: 'Sign up',
-      icon: 'lock_outline',
-      anonymousAccess: true,
-      hideIfLogged: true,
-      route: {
-        path: '/signup',
-        component: SignUp
-      }
-    }
-  ])
-
-  let onServiceDestroy = () => {
-    Vue.prototype.$services.emit('app:menu:remove', [
+  let register = () => {
+    Vue.prototype.$services.emit('app:menu:add', [
       {
-        path: '/signup'
+        title: 'Sign up',
+        icon: 'lock_outline',
+        anonymousAccess: true,
+        hideIfLogged: true,
+        section: 'Connection',
+        route: {
+          path: '/signup',
+          component: SignUp
+        }
       }
     ])
 
-    Vue.prototype.$services.emit('service:destroy:auth:done')
+    let onServiceDestroy = () => {
+      Vue.prototype.$services.emit('app:menu:remove', [
+        {
+          path: '/signup'
+        }
+      ])
+
+      Vue.prototype.$services.emit('service:destroy:auth:done')
+    }
+
+    Vue.prototype.$services.once('service:destroy:auth', onServiceDestroy)
   }
 
-  Vue.prototype.$services.once('service:destroy:auth', onServiceDestroy)
+  if (Vue.prototype.$services.appReady) {
+    register()
+  } else {
+    Vue.prototype.$services.once('app:ready', register)
+  }
 }
